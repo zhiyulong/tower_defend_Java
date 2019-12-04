755b20aee4b9a9557b3482c7ed512d72f2882041
Mark Hardy <markhardy@email.arizona.edu>
2019-12-04 14:00:17 -0700

    Enemy deaths
    Added new class for Bullets fired at enemies and methods to detect
    positions of enemies and bullets on the scene.

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,5 +1,18 @@
+import javafx.geometry.Bounds;
 import javafx.scene.shape.Circle;
 
 public class Bullet extends Circle {
+	
+	public double getX() {
+		Bounds coordinates = this.localToScene(this.getBoundsInLocal());
+		
+		return coordinates.getMinX();
+	}
+	
+	public double getY() {
+		Bounds coordinates = this.localToScene(this.getBoundsInLocal());
+		
+		return coordinates.getMinY();
+	}
 
 }
--- src/Enemie.java
+++ src/Enemie.java
@@ -1,11 +1,18 @@
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+
 import javafx.animation.Animation;
 import javafx.animation.TranslateTransition;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
-public class Enemie {
+public class Enemie extends Node {
 	
 	private int enemieID;
 	private ImageView image;
@@ -43,6 +50,46 @@ public class Enemie {
 	
 	public void subBlood(int num) {
 		blood -= num;
+	}
+	
+	public double getX() {
+		Bounds coordinates = this.localToScene(this.getBoundsInLocal());
+		
+		return coordinates.getMinX();
+	}
+	
+	public double getY() {
+		Bounds coordinates = this.localToScene(this.getBoundsInLocal());
+		
+		return coordinates.getMinY();
+	}
+
+
+	@Override
+	protected boolean impl_computeContains(double arg0, double arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public BaseBounds impl_computeGeomBounds(BaseBounds arg0, BaseTransform arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	protected NGNode impl_createPeer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Object impl_processMXNode(MXNodeAlgorithm arg0, MXNodeAlgorithmContext arg1) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
