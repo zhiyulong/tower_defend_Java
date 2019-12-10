6e1ba46b7b9922615625544b8dae82870167ac28
donshawhu <42550177+donshawhu@users.noreply.github.com>
2019-12-09 23:21:49 -0700

    Update TowerDefense.java

--- src/TowerDefense.java
+++ src/TowerDefense.java
@@ -3,15 +3,9 @@ import javafx.application.Application;
 public class TowerDefense {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 //		////Application.launch(View.class, args);
 		Application.launch(Menu.class, args);
-=======
-		
-		Application.launch(View.class, args);
-		//Application.launch(Menu.class, args);
->>>>>>> branch 'master' of https://github.com/csc335-fall-2019/csc335-towerdef-markhardy-zhiyulong-donshawhu-jiaxukang.git
 
 	}
 
